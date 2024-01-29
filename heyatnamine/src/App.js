fc9b8a86584f34c83cb9387ea48683952742c1f2
code.polyglot <rahman.dev.niftaliyev@gmail.com>
2024-01-29 08:20:15 +0400

    add footer and dark light theme

--- heyatnamine/src/App.js
+++ heyatnamine/src/App.js
@@ -1,19 +1,35 @@
 import React from "react";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import WithAction from "./WithAction";
 import CallToActionWithVideo from "./CallToActionWithVideo";
 import SplitWithImage from "./SplitWithImage";
 import WithSpeechBubbles from "./WithSpeechBubbles";
 import ThreeTierPricing from "./ThreeTierPricing";
+import LargeWithAppLinksAndSocial from "./LargeWithAppLinksAndSocial";
+
+// Define your custom theme object
+const theme = extendTheme({
+  // Your theme configuration goes here
+  // Example:
+  colors: {
+    primary: "#3182ce",
+    secondary: "#4a5568",
+  },
+  fonts: {
+    body: "Roboto, sans-serif",
+    heading: "Montserrat, sans-serif",
+  },
+});
 
 const App = () => {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <WithAction />
       <CallToActionWithVideo />
       <SplitWithImage />
       <WithSpeechBubbles />
       <ThreeTierPricing />
+      <LargeWithAppLinksAndSocial />
     </ChakraProvider>
   );
 };
